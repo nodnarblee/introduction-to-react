4f192b0d627facacf55407b030c0422094cef12b
Brandon <brandon@sidebuy.com>
2016-02-20 16:35:20 -0800

    fixed parent props

--- src/components/app.js
+++ src/components/app.js
@@ -5,11 +5,12 @@ import ToDoForm from './todo_form.js';
 export default React.createClass({
     render() {
         let name = `${this.props.name}`
+        let tasks = ['Buy Milk', 'Clean Room', 'Pay Phone Bill', 'Learn ReactJS'];
         return (
             <div>
                 <h1>{ name }'s todo list</h1>
                 < ToDoForm />
-                < ToDoList />
+                < ToDoList tasks={tasks}/>
             </div>
         )
     }
--- src/components/todo_list.js
+++ src/components/todo_list.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 export default React.createClass({
     render() {
-        var tasks = ['Buy Milk', 'Clean Room', 'Pay Phone Bill', 'Learn ReactJS'];
         return (
             <ul>
-            { tasks.map((item, index)=>{ return (<li key={index}>{item}</li>) })}
+            { this.props.tasks.map((item, index)=>{ return (<li key={index}>{item}</li>) })}
             </ul>
         )
     }
